aea0658db2d0e94df52180d57e72cc7104346413
Adarsh <adarsh.hiworth@gmail.com>
2023-12-04 19:56:02 +0530

    sales details and create new order and report

--- pharmacy_mgmnt/models/stock_view_and_order.py
+++ pharmacy_mgmnt/models/stock_view_and_order.py
@@ -11,7 +11,7 @@ class StockViewOrder(models.Model):
 
     sl_no = fields.Char(string='sl no')
     name = fields.Many2one("res.partner", string="Supplier", domain="[('supplier', '=', True)]")
-    med_category = fields.Selection([('indian', 'Indian'), ('german', 'German')],string="Made In")
+    med_category = fields.Selection([('indian', 'Indian'), ('german', 'German')], string="Made In")
     group_id = fields.Many2one("product.medicine.group", string="Group")
     potency_id = fields.Many2one("product.medicine.subcat", string="Potency")
     packing_id = fields.Many2one("product.medicine.packing", string="Packing")
@@ -21,7 +21,8 @@ class StockViewOrder(models.Model):
     date_to = fields.Date(string="Date To")
     stock_view_ids = fields.One2many("stock.view.order.lines", "stock_view_line_id")
     order_ids = fields.One2many("stock.order.lines", "stock_order_line_id")
-    sales_order_ids = fields.One2many("sales.order.lines" ,"sales_order_line_id")
+    sales_order_ids = fields.One2many("sales.order.lines", "sales_order_line_id")
+    purchase_order_ids = fields.One2many("stock.purchase.order.lines", "purchase_order_line_id")
     date_field = fields.Date(string='Date', default=fields.Date.today)
     state = fields.Selection([('draft', 'Draft'), ('order', 'Order'), ('purchased', 'Purchased')]
                              , required=True, default='draft')
@@ -31,9 +32,10 @@ class StockViewOrder(models.Model):
         if vals.get('sl_no', 'New') == 'New':
             vals['sl_no'] = self.env['ir.sequence'].next_by_code(
                 'stock.order') or 'New'
-            print(vals,'valsssssssssssssssssssssssssss')
-        result = super( StockViewOrder, self).create(vals)
+            print(vals, 'valsssssssssssssssssssssssssss')
+        result = super(StockViewOrder, self).create(vals)
         return result
+
     @api.multi
     def order_purchased(self):
         if self.state == "order":
@@ -46,7 +48,7 @@ class StockViewOrder(models.Model):
             for rec in self.stock_view_ids:
                 if rec.number_of_order != 0:
                     new_lines.append((0, 0, {
-                        'medicine_1': rec.medicine_1.id,
+                        # 'medicine_1': rec.medicine_1.id,
                         'medicine_id': rec.medicine_id.id,
                         'rack': rec.rack.id,
                         'potency': rec.potency.id,
@@ -62,6 +64,7 @@ class StockViewOrder(models.Model):
                     # self.write({'order_ids': new_lines})
                     rec.number_of_order = 0
             self.order_ids = new_lines
+
     @api.multi
     def print_stock_order_report(self):
         if self.stock_view_ids:
@@ -127,7 +130,6 @@ class StockViewOrder(models.Model):
                     # self.write({'order_ids': new_lines})
             self.order_ids = new_lines
 
-
         domain = []
         if self.name:
             domain += [('supplier_id', '=', self.name.id)]
@@ -150,50 +152,99 @@ class StockViewOrder(models.Model):
         for rec in self:
             if rec.stock_view_ids:
                 rec.stock_view_ids = [(5, 0, 0)]
-            # rec.account_id = 25
-            # rec.stock_view_ids = []
+
             list = []
             stock_items = self.env['entry.stock'].search(domain)
+
             if stock_items:
                 for line in stock_items:
-                    list.append([0, 0, {'medicine_1': line.id,
-                                        'medicine_id': line.medicine_1.id,
-                                        'rack': line.rack.id,
-                                        'potency': line.potency.id,
-                                        'medicine_name_packing': line.medicine_name_packing.id,
-                                        # 'company': line.company.id,
-                                        'medicine_grp1': line.medicine_grp1.id,
-                                        'qty': line.qty,
-                                        'mrp': line.mrp,
-                                        'batch_2': line.batch_2,
-                                        'manf_date': line.manf_date,
-                                        'expiry_date': line.expiry_date,
-                                        }
-                                 ])
+                    found_duplicate = False
+
+                    for item in list:
+                        if (
+                                line.medicine_1.id == item[2]['medicine_id'] and
+                                line.potency.id == item[2]['potency'] and
+                                line.medicine_name_packing.id == item[2]['medicine_name_packing'] and
+                                line.medicine_grp1.id == item[2]['medicine_grp1'] and
+                                line.company.id == item[2]['company']
+                        ):
+                            item[2]['qty'] += line.qty
+                            found_duplicate = True
+                            if line.expiry_date <= fields.Date.today():
+                                item[2]['ex_qty'] += line.qty
+                            break
+
+                    if not found_duplicate:
+                        if line.expiry_date <= fields.Date.today():
+                            ex_qty = line.qty
+                        else:
+                            ex_qty = 0
+                        list.append([0, 0, {
+                            'medicine_id': line.medicine_1.id,
+                            'rack': line.rack.id,
+                            'company': line.company.id,
+                            'potency': line.potency.id,
+                            'medicine_name_packing': line.medicine_name_packing.id,
+                            'medicine_grp1': line.medicine_grp1.id,
+                            'qty': line.qty,
+                            'ex_qty': ex_qty,
+                            'mrp': line.mrp,
+                            'batch_2': line.batch_2,
+                            'manf_date': line.manf_date,
+                            'expiry_date': line.expiry_date,
+                        }])
+
             rec.stock_view_ids = list
             domain = []
 
+            # for rec in self:
+        #     if rec.stock_view_ids:
+        #         rec.stock_view_ids = [(5, 0, 0)]
+            # rec.account_id = 25
+            # rec.stock_view_ids = []
+            list = []
+            # stock_items = self.env['entry.stock'].search(domain)
+            # if stock_items:
+            #     for line in stock_items:
+            #             list.append([0, 0, {
+            #                 'medicine_id': line.medicine_1.id,
+            #                 'rack': line.rack.id,
+            #                 'potency': line.potency.id,
+            #                 'medicine_name_packing': line.medicine_name_packing.id,
+            #                 # 'company': line.company.id,
+            #                 'medicine_grp1': line.medicine_grp1.id,
+            #                 'qty': line.qty,
+            #                 'mrp': line.mrp,
+            #                 'batch_2': line.batch_2,
+            #                 'manf_date': line.manf_date,
+            #                 'expiry_date': line.expiry_date,
+            #             }])
+            # rec.stock_view_ids = list
+            # domain = []
+
     @api.multi
-    def get_sales(self):
+    def get_purchase(self):
         domain = []
         for rec in self.stock_view_ids:
-            if rec.get_sales == True:
-                # domain = [('type', '=', 'out_invoice')]
+            if rec.get_purchase == True:
+                domain = [('invoice_id.type', '=', 'in_invoice'),('invoice_id.state', '=', 'paid')]
                 if rec.medicine_id.id:
                     domain += [('product_id', '=', rec.medicine_id.id)]
-                if rec.medicine_id.id:
+                if rec.potency.id:
                     domain += [('medicine_name_subcat', '=', rec.potency.id)]
-                if rec.medicine_id.id:
+                if rec.company.id:
                     domain += [('product_of', '=', rec.company.id)]
-                if rec.medicine_id.id:
+                if rec.medicine_grp1.id:
                     domain += [('medicine_grp', '=', rec.medicine_grp1.id)]
-                if rec.medicine_id.id:
-                    domain += [('expiry_date', '=', rec.expiry_date)]
-                if rec.medicine_id.id:
-                    domain += [('manf_date', '=', rec.manf_date)]
+                if self.date_from:
+                    domain += [('invoice_id.date_invoice', '>=', self.date_to)]
+                if self.date_to:
+                    domain += [('invoice_id.date_invoice', '<=', self.date_from)]
+                if self.name:
+                    domain += [('invoice_id.partner_id', '<=', self.name.id)]
         for rec in self:
-            if rec.sales_order_ids:
-               rec.sales_order_ids = [(5, 0, 0)]
+            if rec.purchase_order_ids:
+                rec.purchase_order_ids = [(5, 0, 0)]
             # rec.account_id = 25
             # rec.sales_order_ids = []
             list = []
@@ -202,32 +253,96 @@ class StockViewOrder(models.Model):
                 for line in invoices:
                     if line.invoice_id.state == 'paid':
                         list.append([0, 0, {
-                                            'number2': line.invoice_id.number2,
-                                            'partner_id': line.invoice_id.partner_id.id,
-                                            'date_invoice': line.invoice_id.date_invoice,
-                                            'quantity': line.quantity,
-                                            'product_id': line.product_id.id,
-                                            'product_of': line.product_of.id,
-                                            'medicine_grp': line.medicine_grp.id,
-                                            'medicine_name_subcat': line.medicine_name_subcat.id,
-                                            }
-                        ])
+                            'invoice_id':line.invoice_id.id,
+                            'partner_id': line.invoice_id.partner_id.id,
+                            'date_invoice': line.invoice_id.date_invoice,
+                            'quantity': line.quantity,
+                            'product_id': line.product_id.id,
+                            'product_of': line.product_of.id,
+                            'medicine_grp': line.medicine_grp.id,
+                            'medicine_name_subcat': line.medicine_name_subcat.id,
+                            'batch_2':line.batch_2
+                        }
+                                     ])
+            rec.purchase_order_ids = list
+            domain = []
+        for rec in self.stock_view_ids:
+            if rec.get_purchase == True:
+                rec.get_purchase = False
+
+    @api.multi
+    def get_sales(self):
+        domain = []
+        for rec in self.purchase_order_ids:
+            if rec.get_sales == True:
+                domain = [('invoice_id.type', '=', 'out_invoice'),('invoice_id.state', '=', 'paid')]
+                if rec.product_id.id:
+                    domain += [('product_id', '=', rec.product_id.id)]
+                if rec.medicine_name_subcat.id:
+                    domain += [('medicine_name_subcat', '=', rec.medicine_name_subcat.id)]
+                if rec.product_of.id:
+                    domain += [('product_of', '=', rec.product_of.id)]
+                if rec.medicine_grp.id:
+                    domain += [('medicine_grp', '=', rec.medicine_grp.id)]
+                if rec.batch_2.id:
+                    domain += [('medicine_grp', '=', rec.batch_2.id)]
+                if self.date_from:
+                    domain += [('invoice_id.date_invoice', '>=', self.date_to)]
+                if self.date_to:
+                    domain += [('invoice_id.date_invoice', '<=', self.date_from)]
+        for rec in self:
+            if rec.sales_order_ids:
+                rec.sales_order_ids = [(5, 0, 0)]
+            # rec.account_id = 25
+            # rec.sales_order_ids = []
+            list = []
+            invoices = self.env['account.invoice.line'].search(domain)
+            if invoices:
+                for line in invoices:
+                    if line.invoice_id.state == 'paid':
+                        list.append([0, 0, {
+                            'invoice_id': line.invoice_id.id,
+                            'partner_id': line.invoice_id.partner_id.id,
+                            'date_invoice': line.invoice_id.date_invoice,
+                            'quantity': line.quantity,
+                            'product_id': line.product_id.id,
+                            'product_of': line.product_of.id,
+                            'medicine_grp': line.medicine_grp.id,
+                            'medicine_name_subcat': line.medicine_name_subcat.id,
+                        }
+                                     ])
             rec.sales_order_ids = list
             domain = []
+        for rec in self.purchase_order_ids:
+            if rec.get_sales == True:
+                rec.get_sales = False
 
 
 class StockViewOrderLine(models.Model):
     _name = "stock.view.order.lines"
     _description = 'Stock View Line'
-    _inherits = {'entry.stock': 'medicine_1'}
-
+    # _inherits = {'entry.stock': 'medicine_1'}
 
     stock_view_line_id = fields.Many2one("stock.view.order", string="Medicine Entry")
-    medicine_1 = fields.Many2one('entry.stock')
-    medicine_id = fields.Many2one('product.product', string="Medicine")
+    # medicine_1 = fields.Many2one('entry.stock')
     number_of_order = fields.Integer("New Order")
-    get_sales = fields.Boolean(default=False,string="Get Sales")
+    # get_sales = fields.Boolean(default=False, string="Sales Details")
+    get_purchase = fields.Boolean(default=False, string="Purchase Details")
     expiry_alert_date = fields.Date(compute='_compute_expiry_alert_date', string='Expiry Alert Date', store=True)
+    ex_qty = fields.Integer(string="Expired Qty")
+
+
+    medicine_id = fields.Many2one('product.product', string="Medicine")
+    rack = fields.Many2one('product.medicine.types', string="rack")
+    company = fields.Many2one('product.medicine.responsible', string="company")
+    potency = fields.Many2one('product.medicine.subcat', string="potency")
+    medicine_name_packing = fields.Many2one('product.medicine.packing', string="medicine_name_packing")
+    medicine_grp1 = fields.Many2one('product.medicine.group', string="medicine_grp1")
+    qty = fields.Integer(string="qty")
+    mrp = fields.Float(string="mrp")
+    batch_2 = fields.Many2one('med.batch', string="batch_2")
+    manf_date = fields.Date()
+    expiry_date = fields.Date()
 
     @api.depends('expiry_date')
     def _compute_expiry_alert_date(self):
@@ -235,16 +350,15 @@ class StockViewOrderLine(models.Model):
             if record.expiry_date:
                 expiry_date = datetime.strptime(record.expiry_date, '%Y-%m-%d').date()
                 record.expiry_alert_date = expiry_date - timedelta(days=180)
-                print('hello', record.expiry_alert_date)
-                # print('hi')
+                # print('hello', record.expiry_alert_date)
             else:
                 record.expiry_alert_date = False
+
 
 class StockOrderLine(models.Model):
     _name = "stock.order.lines"
     _description = 'Stock Order Line'
     _inherits = {'entry.stock': 'medicine_1'}
-
 
     stock_order_line_id = fields.Many2one("stock.view.order", string="Medicine Entry")
     medicine_1 = fields.Many2one('entry.stock')
@@ -252,14 +366,16 @@ class StockOrderLine(models.Model):
     new_order = fields.Integer(string="New Order")
 
 
+
+
+
 class SalesOrderLine(models.Model):
     _name = "sales.order.lines"
     _description = 'Sales Order Line'
 
-
     sales_order_line_id = fields.Many2one("stock.view.order", string="Medicine Entry")
-    number2 = fields.Char("Invoice Number")
-    partner_id = fields.Many2one("res.partner","Customer")
+    invoice_id = fields.Many2one("account.invoice", "Invoice",domain=[('type','=','out_invoice')])
+    partner_id = fields.Many2one("res.partner", "Customer")
     date_invoice = fields.Date("Date")
     quantity = fields.Integer("quantity")
     product_id = fields.Many2one("product.product")
@@ -267,6 +383,45 @@ class SalesOrderLine(models.Model):
     medicine_grp = fields.Many2one("product.medicine.group")
     medicine_name_subcat = fields.Many2one("product.medicine.subcat")
 
+    @api.multi
+    def open_invoice(self):
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        redirect_url = "%s/web#id=%d&view_type=form&model=account.invoice&menu_id=331&action=400" % (
+            base_url, self.invoice_id.id)
+        return {
+            'type': 'ir.actions.act_url',
+            'url': redirect_url,
+            'target': 'self',
+        }
 
+
+
+class StockPurchaseOrderLine(models.Model):
+    _name = "stock.purchase.order.lines"
+    _description = 'Stock purchase Order Line'
+
+    purchase_order_line_id = fields.Many2one("stock.view.order", string="Medicine Entry")
+    get_sales = fields.Boolean(default=False, string="Sales Details")
+    invoice_id = fields.Many2one("account.invoice", "Invoice", domain=[('type', '=', 'in_invoice')])
+
+    partner_id = fields.Many2one("res.partner", "Supplier")
+    date_invoice = fields.Date("Date")
+    quantity = fields.Integer("quantity")
+    product_id = fields.Many2one("product.product")
+    product_of = fields.Many2one("product.medicine.responsible")
+    medicine_grp = fields.Many2one("product.medicine.group")
+    medicine_name_subcat = fields.Many2one("product.medicine.subcat")
+    batch_2 = fields.Many2one('med.batch','Batch')
+
+    @api.multi
+    def open_invoice(self):
+        base_url = self.env['ir.config_parameter'].sudo().get_param('web.base.url')
+        redirect_url = "%s/web#id=%d&view_type=form&model=account.invoice&menu_id=328&action=399" % (
+            base_url, self.invoice_id.id)
+        return {
+            'type': 'ir.actions.act_url',
+            'url': redirect_url,
+            'target': 'self',
+        }
 
 
