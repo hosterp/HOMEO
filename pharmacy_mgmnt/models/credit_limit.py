a05ba525f4828c4e877ea758f0f46deced40967f
Libiya <libiyasebastianhiworth@gmail.com>
2024-03-18 11:06:42 +0530

    onchange function added to creditdays in the customer creation page

--- pharmacy_mgmnt/models/credit_limit.py
+++ pharmacy_mgmnt/models/credit_limit.py
@@ -34,3 +34,28 @@ class CreditLimitCustomer(models.Model):
     used_credit_amt = fields.Float('Used Amount', compute="_compute_credited_amt")
     days_credit_limit = fields.Float('Credit Limit Days')
     credit_end_date = fields.Date(string="Credit end Date")
+
+    @api.onchange('days_credit_limit')
+    def compute_credit_end_date(self):
+        for rec in self:
+            if rec.days_credit_limit:
+                last_payment = self.env['partner.payment'].search(
+                    [('partner_id', '=', rec.id), ('balance_amount', '=', 0)],
+                    order='id desc', limit=1)
+
+                if last_payment.date:
+                    pass
+                else:
+                    date_today = fields.Date.today()
+                    x = datetime.strptime(date_today, '%Y-%m-%d')
+                    next_date = x + relativedelta(days=rec.days_credit_limit)
+                    cal_date = datetime.strftime(next_date, '%Y-%m-%d')
+                    rec.credit_end_date = cal_date
+
+    # def _compute_credited_amt(self):
+    #     for rec in self:
+    #         customer_invoices = rec.env['account.invoice'].search([('pay_mode', '=', 'credit'),
+    #                                                                ('partner_id', '=', rec.id),
+    #                                                                ('type', '=', 'out_invoice'),
+    #                                                                ('state', '=', 'open')])
+    #         rec.used_credit_amt = sum(customer_invoices.mapped('amount_total'))
