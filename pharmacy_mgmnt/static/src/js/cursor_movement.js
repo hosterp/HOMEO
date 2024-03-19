2617b3b4c5ccef0a57806db31dc54dd427eb7532
Adarsh <adarsh.hiworth@gmail.com>
2024-03-19 12:00:57 +0530

    select first item in many2one

--- pharmacy_mgmnt/static/src/js/cursor_movement.js
+++ pharmacy_mgmnt/static/src/js/cursor_movement.js
@@ -17,12 +17,23 @@ $(document).ready(function(){
             $this.prop('selectionStart', $this.val().length);
         }
     });
-    $(document).on('keydown', '.oe_form_field_many2one', function(event) {
-        var $currentField = $(this);
-        var $createLabelElement = $('.oe_view_manager_body').find('li.ui-menu-item:first a:contains("Create")');
-        if ($createLabelElement.length > 0 && event.which === $.ui.keyCode.ENTER) {
-            $createLabelElement.click();
+//    $(document).on('keydown', '.oe_form_field_many2one', function(event) {
+//        var $currentField = $(this);
+//        var $createLabelElement = $('.oe_view_manager_body').find('li.ui-menu-item:first a:contains("Create")');
+//        if ($createLabelElement.length > 0 && event.which === $.ui.keyCode.ENTER) {
+//            $createLabelElement.click();
+//        }
+//    });
+    $(document).on('focus', '.oe_form_field_many2one', function(event) {
+        if (!$(this).data('clicked')) { // Check if the click event hasn't been triggered yet
+            $(this).data('clicked', true); // Set the flag to indicate the click event has been triggered
+            $(this).find('.oe_m2o_drop_down_button').click(); // Trigger click event on oe_m2o_drop_down_button
         }
     });
+
+    $(document).on('blur', '.oe_form_field_many2one', function(event) {
+        $(this).data('clicked', false); // Reset the flag when the field loses focus
+    });
+
 
 });
