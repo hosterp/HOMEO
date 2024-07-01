5b74b3d467d625bf707da0e3f91df505e853dae0
Adarsh <adarsh.hiworth@gmail.com>
2024-07-01 18:07:28 +0530

    create product

--- pharmacy_mgmnt/static/src/js/cursor_movement.js
+++ pharmacy_mgmnt/static/src/js/cursor_movement.js
@@ -76,7 +76,7 @@ $(document).on('focus', '.oe_form_field_many2one', function(event) {
 
 $(document).on('focus', '.oe_form_field_many2one[data-fieldname="product_id"]', function(event) {
     var $productField = $(this);
-    var $ul = $productField.closest('tr').find("ul.ui-autocomplete:eq(22)");
+    var $ul = $productField.closest('tr').find("ul.ui-autocomplete:eq(21)");
     var $firstItem = $ul.find("li:first");
     if ($firstItem.length && !$firstItem.data('clicked')) {
         $firstItem.trigger("click");
