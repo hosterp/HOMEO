be142b3c16daf1f91c96c9a61c4cf4149a82f0f1
Adarsh <adarsh.hiworth@gmail.com>
2024-07-03 10:31:47 +0530

    required class

--- pharmacy_mgmnt/models/account_invoice.py
+++ pharmacy_mgmnt/models/account_invoice.py
@@ -1095,7 +1095,7 @@ class AccountInvoiceLine(models.Model):
         return latest_entry_stock.company.id if latest_entry_stock and latest_entry_stock.company else False
 
 
-    medicine_name_subcat = fields.Many2one('product.medicine.subcat', 'Potency', required=True,)
+    medicine_name_subcat = fields.Many2one('product.medicine.subcat', 'Potency')
     medicine_name_packing = fields.Many2one('product.medicine.packing', 'Pack',default=lambda self: self._get_default_medicine_pack())
 
     # medicine_grp = fields.Many2one('product.medicine.group', 'GROUP',compute='_compute_taxes',readonly="0")
