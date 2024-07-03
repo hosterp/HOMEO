c09425da33be9653e76968db1063029f51431d1d
Libiya <libiyasebastianhiworth@gmail.com>
2024-07-03 10:17:22 +0530

    js file updated

--- pharmacy_mgmnt/static/src/js/cursor_movement.js
+++ pharmacy_mgmnt/static/src/js/cursor_movement.js
@@ -128,9 +128,7 @@ $(document).on('focus', '.oe_form_field_many2one[data-fieldname="medicine_rack"]
     });
 });
 
-$(document).ready(function() {
-    $(document).on('click', '.oe_button.oe_form_button_edit', function() {
-          alert('clicked');
-    });
-  
-});
+
+
+
+
