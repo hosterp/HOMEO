a72bacfab77f9d0841666b8a6f5f4a2846ed07d5
Libiya <libiyasebastianhiworth@gmail.com>
2024-08-20 17:54:02 +0530

    Dash board should contain count of low stock, count of Expiry Items, count of Cheque Collection.count added

--- pharmacy_mgmnt/models/home_page.py
+++ pharmacy_mgmnt/models/home_page.py
@@ -5,8 +5,33 @@ class HomePage(models.Model):
 
 
     name=fields.Char('Name')
+    stock_count_expired = fields.Integer(string='Stock Count Expired', compute='_compute_stock_count')
+    low_stock_count = fields.Integer(string='Low Stock Count', compute='_compute_stock_low_count')
+    cheque_count = fields.Integer(string='Cheque Count', compute='_compute_cheque_count')
 
+    @api.depends('stock_count_expired')
+    def _compute_stock_count(self):
+        stock_model = self.env['entry.stock']
+        count = stock_model.search_count([('expiry_date', '<=', fields.Date.today())])
+        for record in self:
+            record.stock_count_expired = count
+            print(record.stock_count_expired,'count')
 
+    @api.depends('low_stock_count')
+    def _compute_stock_low_count(self):
+        stock_model = self.env['entry.stock']
+        count = stock_model.search_count([('qty','<=','50')])
+        for record in self:
+            record.low_stock_count = count
+            print(record.low_stock_count, 'count')
+
+    @api.depends('cheque_count')
+    def _compute_cheque_count(self):
+        stock_model = self.env['cheque.entry']
+        count = stock_model.search_count([('deposit_date', '=', fields.Date.today())])
+        for record in self:
+            record.cheque_count = count
+            print(record.cheque_count, 'count')
 
     def call_count_of_low_stock(self,cr,uid,ids,context):
         mod_obj = self.pool.get('ir.model.data')
