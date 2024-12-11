b060b6fae8b7e4929080195c93cfa1edeaea1f8d
Libiya <libiyasebastianhiworth@gmail.com>
2024-12-11 10:41:29 +0530

    customer invoice payment mode order changed and Cheque added to the dropdownlist

--- pharmacy_mgmnt/models/account_invoice.py
+++ pharmacy_mgmnt/models/account_invoice.py
@@ -2872,7 +2872,7 @@ class AccountInvoice(models.Model):
     password = fields.Char(Required=True, )
     create_bool = fields.Boolean(Default=False)
     paid_bool = fields.Boolean('Invoice Paid?')
-    pay_mode = fields.Selection([('credit', 'Credit'),('cash', 'Cash'), ('upi', 'UPI'),('card','Card')], 'Payment Mode',
+    pay_mode = fields.Selection([('cash', 'Cash'),('upi', 'UPI'),('card','Card'),('credit', 'Credit'),('cheque','Cheque')], 'Payment Mode',
                                 default='cash')
     amount_in_words = fields.Char('Amount in Words', compute='_compute_amount_in_words')
     phone_number = fields.Char('Phone No',size=10,related="partner_id.mobile")
